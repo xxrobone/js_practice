40fb00aa4db74d83f16bf0ac3c1ffbaf29f4e384
xxrobone <robert.wagar@edu.cmeducations.se>
2023-05-20 18:30:59 +0200

    fun learning alot :D

--- index.js
+++ index.js
@@ -1,68 +1,147 @@
+// Number of people on the bus
+let number = function (busStops) {
+  // Good Luck!
+  let passangers = 0;
+  for (let inOut of busStops) {
+    passangers += inOut[0] - inOut[1];
+  }
+  return passangers;
+};
 
-// find needle in a haystack array of words containing word needle
-const wordsArr = ['hay', 'poop', 'frog', 'code', 'banana', 'needle', 'city']
+// codewars DNA strand replace
+function DNAstrand(dna) {
+  let swap = '';
+  dna
+    .split('')
+    .map((e) => {
+      if (e === 'G') {
+        e = 'C';
+        swap += e;
+      } else if (e === 'C') {
+        e = 'G';
+        swap += e;
+      } else if (e === 'T') {
+        e = 'A';
+        swap += e;
+      } else if (e === 'A') {
+        e = 'T';
+        swap += e;
+      } else {
+        swap += e;
+      }
+    })
+    .join('');
 
-function findNeedle(haystack) {
-    let msg = `found the needle at position `
-    let needle = (n) => n === 'needle'
-    return msg + haystack.findIndex(needle)
+  return swap;
 }
 
-console.log(findNeedle(wordsArr))
+console.log(DNAstrand('TATAZZZGCGC'));
+
+// creative hello world
+function greet(str) {
+  let arr = str.split(' ');
+  let arr2 = arr.map((w) => w.split('').reverse());
+  console.log(arr2);
+  let w1 = '';
+  let w2 = '';
+  for (let i = 0; i < arr2[0].length; i++) {
+    w1 += `\n ${arr2[0][i]}`;
+  }
+  for (let j = arr2[1].length - 1; j >= 0; j--) {
+    w2 += `\n ${arr2[1][j]}`;
+  }
+  console.log(`${w1.toUpperCase()} \n ${w2.toUpperCase()}`);
+}
+
+// greet('hello world!');
+
+// greet2 = h => 'hello world!'
+// console.log(greet2())
+
+// get the sum an array excluding the highest and lowest numbers
+const nArray = [3, 2, 5, 6, 8, 8, 1, 1];
+
+function sumArray(array) {
+  return Array.isArray(array) && array.length >= 2
+    ? array
+        .sort((a, b) => a - b)
+        .slice(1, -1)
+        .reduce((acc, curr) => acc + curr, 0)
+    : 0;
+}
+
+// console.log(sumArray(nArray))
+
+// remove first and last character
+function removeFirstAndLast(str) {
+  return str.slice(1, str.length - 1);
+}
+
+// console.log(removeFirstAndLast('babaloola'))
+// find needle in a haystack array of words containing word needle
+const wordsArr = ['hay', 'poop', 'frog', 'code', 'banana', 'needle', 'city'];
+
+function findNeedle(haystack) {
+  let msg = `found the needle at position `;
+  let needle = (n) => n === 'needle';
+  return msg + haystack.findIndex(needle);
+}
+
+//console.log(findNeedle(wordsArr))
 
 function numReverse(n) {
-  return n.toString().split('').reverse().map(Number)
+  return n.toString().split('').reverse().map(Number);
 }
 
 // console.log(numReverse(35231))
 
 // return a number to negative
 function makeNegative(num) {
-  return num<0 ? num : -num
+  return num < 0 ? num : -num;
 }
 
 // console.log(makeNegative(-3))
-  
+
 // 0.5 liters per hour
 function timeToLiters(hours) {
-  let liters = hours / 2
-  console.log(liters)
-  return Math.floor(liters)
+  let liters = hours / 2;
+  console.log(liters);
+  return Math.floor(liters);
 }
 
 // console.log(timeToLiters(11.8))
 
 const MORSE_CODE = {
-  ".-": "A",
-  "-...": "B",
-  ".--": "W",
-  "--.": "G",
-  "-..": "D",
-  ".": "E",
-  "...-": "V",
-  "--..": "Z",
-  "..": "I",
-  ".---": "J",
-  "-.-": "K",
-  ".-..": "L",
-  "--": "M",
-  "-.": "N",
-  "---": "O",
-  ".--.": "P",
-  ".-.": "R",
-  "...": "S",
-  "-": "T",
-  "..-": "U",
-  "..-.": "F",
-  "....": "H",
-  "-.-.": "C",
-  "--.-": "Q",
-  "-.--": "Y",
-  "-..-": "X"
+  '.-': 'A',
+  '-...': 'B',
+  '.--': 'W',
+  '--.': 'G',
+  '-..': 'D',
+  '.': 'E',
+  '...-': 'V',
+  '--..': 'Z',
+  '..': 'I',
+  '.---': 'J',
+  '-.-': 'K',
+  '.-..': 'L',
+  '--': 'M',
+  '-.': 'N',
+  '---': 'O',
+  '.--.': 'P',
+  '.-.': 'R',
+  '...': 'S',
+  '-': 'T',
+  '..-': 'U',
+  '..-.': 'F',
+  '....': 'H',
+  '-.-.': 'C',
+  '--.-': 'Q',
+  '-.--': 'Y',
+  '-..-': 'X',
 };
 
 // codewars morse code problem
-let decodeMorse = function(morseCode){
+let decodeMorse = function (morseCode) {
   function decodeMorseLetter(letter) {
     return MORSE_CODE[letter];
   }
@@ -70,28 +149,38 @@ let decodeMorse = function(morseCode){
     return word.split(' ').map(decodeMorseLetter).join('');
   }
   return morseCode.trim().split('   ').map(decodeMorseWord).join(' ');
-}
+};
 
 // console.log(decodeMorse('--   ---   .--.'))
 
 // reverse a input of numbers and output it in a descending order
 
-function descendingOrder(n){
-     return Number(n.toString().split("").sort((a, b) => b - a).join(""))
+function descendingOrder(n) {
+  return Number(
+    n
+      .toString()
+      .split('')
+      .sort((a, b) => b - a)
+      .join('')
+  );
 }
 
-console.log(descendingOrder(1347))
+console.log(descendingOrder(1347));
 
 function highToLow(numbers) {
   // splitting the number chain to an array of numbers
-  let numArr = Array.from(String(numbers, Number))
-// low to high
-//  return numArr.sort(function (a, b) { return a - b }).join()
+  let numArr = Array.from(String(numbers, Number));
+  // low to high
+  //  return numArr.sort(function (a, b) { return a - b }).join()
 
-  // from high to low 
-  let reverse = numArr.sort(function (a, b) { return b - a }).join('')
+  // from high to low
+  let reverse = numArr
+    .sort(function (a, b) {
+      return b - a;
+    })
+    .join('');
 
-  return Number(reverse)
+  return Number(reverse);
 }
 // console.log(highToLow(8968308873))
 
@@ -111,7 +200,7 @@ const obj = {
     /* const that = this */
     const inner = function () {
       /*  console.log(that.x) */
-     // console.log(this.x);
+      // console.log(this.x);
     };
     inner.bind(this)();
   },
@@ -134,7 +223,7 @@ const getNonCharRepeatingString = (str) => {
     } else {
       subStr += current;
     }
-   // console.log('check', subStr);
+    // console.log('check', subStr);
   }
   strArr.push(subStr);
 
@@ -149,7 +238,7 @@ const getNonCharRepeatingString = (str) => {
       return b.length > a.length ? b : a;
     }, '');
 
- // console.log(`longest using reduce: ${getLongest(strArr)}`);
+  // console.log(`longest using reduce: ${getLongest(strArr)}`);
 
   return console.log('this is the longest string: ', longest);
 };
